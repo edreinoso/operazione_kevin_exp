72bef2ede29835b5aeb5454f6ae1dc2f09bf26f4
igormazurek <60849909+igormazurek@users.noreply.github.com>
2021-12-21 23:38:46 +0100

    boxplot update

--- latency/latency_plot.py
+++ latency/latency_plot.py
@@ -1,9 +1,11 @@
 import os, subprocess, sys, time
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
-
 import matplotlib as mpl
-mpl.rcParams['figure.dpi'] = 300
+import matplotlib.patches as mpatches
+
+mpl.rcParams['figure.dpi'] = 200
 #load data
 f = open('get_latency_output1.txt')
 get_list1 = f.read().splitlines()
@@ -23,11 +25,29 @@ put_list2 = [float(i) for i in put_list2]
 
 
 
-df = pd.DataFrame({'Get - sync before read': get_list1,'Put - sync before read': put_list1, 'Get - sync at write': get_list2, 'Put - sync at write': put_list2})
+df = pd.DataFrame({'Get - sync before read': get_list1, 'Get - sync at write': get_list2, 'Put - sync before read': put_list1, 'Put - sync at write': put_list2})
 print(df.describe())
+c = "black"
+ax = df.boxplot(patch_artist=True, boxprops=dict(facecolor=c, color=c),
+                capprops=dict(color=c),
+                whiskerprops=dict(color=c),
+                flierprops=dict(color=c, markeredgecolor=c),
+                medianprops=dict(color=c), )
+#plt.suptitle("Latency")
+plt.xticks([])
+plt.xlabel("Get                                               Put")
+plt.ylabel("Time (seconds)")
 
-df.boxplot().set_ylabel('Time (seconds)')
-plt.suptitle("Latency")
+
+ax.findobj(matplotlib.patches.Patch)[0].set_facecolor("firebrick")
+ax.findobj(matplotlib.patches.Patch)[1].set_facecolor("forestgreen")
+ax.findobj(matplotlib.patches.Patch)[2].set_facecolor("firebrick")
+ax.findobj(matplotlib.patches.Patch)[3].set_facecolor("forestgreen")
+
+
+forestgreen_patch = mpatches.Patch(color='forestgreen', label='sync at write')
+firebrick_patch = mpatches.Patch(color='firebrick', label='sync before read')
+plt.legend(handles=[forestgreen_patch, firebrick_patch])
 plt.show()
 
 
